7b0270b5c055ca0ecfe560f58c94df1ae394f14d
bneuman <bneuman@14west.us>
2020-03-04 15:31:13 -0500

    initial commit

---
+++ src/test/java/com/agora/hackathon/team5/AuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agora.hackathon.team5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

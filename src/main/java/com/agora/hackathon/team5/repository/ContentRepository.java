41d29e1d7b58da481bf24d8fdcb5e8de686d8e47
ben <ben@localhost>
2020-03-05 12:42:20 -0500

    hz cache

--- src/main/java/com/agora/hackathon/team5/repository/ContentRepository.java
+++ src/main/java/com/agora/hackathon/team5/repository/ContentRepository.java
@@ -3,9 +3,11 @@ package com.agora.hackathon.team5.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.agora.hackathon.team5.model.Content;
+import org.springframework.cache.annotation.Cacheable;
 
 public interface ContentRepository  extends MongoRepository<Content, Long> {
 
+	@Cacheable("content")
 	public Content findByPubCode(String pubCode);
 
 }
